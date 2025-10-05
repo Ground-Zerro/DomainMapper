7b3190a00afcea36159c3ba762769a98c785ec1a
Ground-Zerro <ksa65s571dozor@gmail.com>
2025-10-06 10:51:51 +1100

    Update Win.bat

--- Windows/Win.bat
+++ Windows/Win.bat
@@ -94,5 +94,3 @@ endlocal
 echo файл скопирован в %UserProfile%\Desktop\domain-ip-resolve.txt
 pause
 exit /b 0
-
-::То-ли при выгрузке на github, то-ли при скачивании с него, в файл как-то попадает BOM... Как это починить я ХЗ.
